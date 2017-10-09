0a950f39faef8a649989fbc046028b6912a7f40e
ShruthiSiva <ssiva@bu.edu>
2017-10-09 19:49:47 -0400

    Latest Changes

---
+++ app/src/main/java/com/example/android/firebaseauthdemo/UserInformation.java
@@ -0,0 +1,20 @@
+package com.example.android.firebaseauthdemo;
+
+/**
+ * Created by shruthisivasubramanian on 9/10/17.
+ */
+
+public class UserInformation {
+
+    public String name;
+    public String address;
+
+    public UserInformation(){
+
+    }
+
+    public UserInformation(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+}
